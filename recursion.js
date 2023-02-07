f76e088aa5992f8eb284eb386bd48f434d3be5fa
Miran-Mirantee <killerm11805@hotmail.com>
2023-02-07 14:53:05 +0700

    Finish mergeSort recursive function

--- recursion.js
+++ recursion.js
@@ -18,5 +18,42 @@ const fibsRec = (n) => {
   return [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
 };
 
+const mergeSort = (arr) => {
+  if (arr.length < 2) return arr;
+  const mid = Math.ceil(arr.length / 2);
+  const left = mergeSort(arr.splice(0, mid));
+  const right = mergeSort(arr.splice(-mid));
+  const newArr = [];
+
+  while (left.length > 0 && right.length > 0) {
+    let first;
+    if (left[0] < right[0]) {
+      first = left.shift();
+    } else {
+      first = right.shift();
+    }
+    newArr.push(first);
+  }
+
+  if (left.length == 0) {
+    newArr.push.apply(newArr, right);
+  } else if (right.length == 0) {
+    newArr.push.apply(newArr, left);
+  }
+
+  return newArr;
+};
+
 console.log(fibs(8));
 console.log(fibsRec(8));
+
+const arr = [8, 4, 5, 6, 7, 3, 1, 2];
+const arr2 = [8, 4, 5, 6, 7, 3, 1, 2, 9];
+const arr3 = [5, 4, 5, 6, 7, 3, 1, 2];
+
+console.log(arr);
+console.log(mergeSort(arr));
+// console.log(arr2);
+// console.log(mergeSort(arr2));
+// console.log(arr3);
+// console.log(mergeSort(arr3));
